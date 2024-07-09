69aba1685c114fc731551b6011133d1789daed49
changin99 <20217140@edu.hanbat.ac.kr>
2024-07-09 21:52:33 +0900

    test

--- 003 Code/Carte/Carte_Daemon/daemon/container.go
+++ 003 Code/Carte/Carte_Daemon/daemon/container.go
@@ -1,16 +1,49 @@
 package daemon
 
 import (
-    "log"
-    "os"
-    "os/exec"
+	"encoding/json"
+	"net/http"
+	"os/exec"
+	"syscall"
 )
 
-func InitContainer(command string) {
-    cmd := exec.Command("sh", "-c", command)
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    if err := cmd.Run(); err != nil {
-        log.Fatal(err)
-    }
+// StartContainer starts a new container process with namespaces and cgroups
+func StartContainer(image, command string) error {
+	cmd := exec.Command("/proc/self/exe", "child", command)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET | syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{ContainerID: 0, HostID: syscall.Getuid(), Size: 1},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{ContainerID: 0, HostID: syscall.Getgid(), Size: 1},
+		},
+	}
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// RunContainer handles the /run endpoint request
+func RunContainer(w http.ResponseWriter, r *http.Request) {
+	var req RunRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := StartContainer(req.Image, req.Command); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"status": "started"})
 }
--- 003 Code/Carte/Carte_Daemon/daemon/namespace.go
+++ 003 Code/Carte/Carte_Daemon/daemon/namespace.go
@@ -1,14 +1,54 @@
 package daemon
 
 import (
-    "os/exec"
-    "syscall"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"syscall"
 )
 
-func SetNamespace() *exec.Cmd {
-    cmd := exec.Command("sh")
-    cmd.SysProcAttr = &syscall.SysProcAttr{
-        Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET,
-    }
-    return cmd
+func setupCgroups() error {
+	cgroups := "/sys/fs/cgroup/"
+	pid := os.Getpid()
+
+	// Memory limit
+	if err := os.MkdirAll(filepath.Join(cgroups, "memory", "carte"), 0755); err != nil {
+		return err
+	}
+	if err := ioutil.WriteFile(filepath.Join(cgroups, "memory", "carte", "memory.limit_in_bytes"), []byte("104857600"), 0700); err != nil {
+		return err
+	}
+	if err := ioutil.WriteFile(filepath.Join(cgroups, "memory", "carte", "cgroup.procs"), []byte(strconv.Itoa(pid)), 0700); err != nil {
+		return err
+	}
+
+	// CPU limit
+	if err := os.MkdirAll(filepath.Join(cgroups, "cpu", "carte"), 0755); err != nil {
+		return err
+	}
+	if err := ioutil.WriteFile(filepath.Join(cgroups, "cpu", "carte", "cpu.cfs_quota_us"), []byte("50000"), 0700); err != nil {
+		return err
+	}
+	if err := ioutil.WriteFile(filepath.Join(cgroups, "cpu", "carte", "cgroup.procs"), []byte(strconv.Itoa(pid)), 0700); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setNamespacesAndCgroups(command string) *exec.Cmd {
+	cmd := exec.Command("sh", "-c", command)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET | syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{ContainerID: 0, HostID: syscall.Getuid(), Size: 1},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{ContainerID: 0, HostID: syscall.Getgid(), Size: 1},
+		},
+	}
+
+	return cmd
 }

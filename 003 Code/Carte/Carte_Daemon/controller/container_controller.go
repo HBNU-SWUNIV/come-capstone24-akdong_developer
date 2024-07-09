315b1713a6ae87045e7f9874395e28dfc6746ac3
changin99 <20217140@edu.hanbat.ac.kr>
2024-07-10 00:15:36 +0900

    test

--- 003 Code/Carte/Carte_Daemon/controller/container_controller.go
+++ 003 Code/Carte/Carte_Daemon/controller/container_controller.go
@@ -1,29 +1,31 @@
 package controller
 
 import (
-	"encoding/json"
-	"net/http"
-
-	"Carte/Carte_Daemon/daemon"
-	"Carte/Carte_Daemon/utils"
+    "encoding/json"
+    "net/http"
+    "Carte_Daemon/daemon"
+    "Carte_Daemon/utils"
+    "log"
 )
 
 type RunRequest struct {
-	Image   string `json:"image"`
-	Command string `json:"command"`
+    Image   string `json:"image"`
+    Command string `json:"command"`
 }
 
 func RunContainer(w http.ResponseWriter, r *http.Request) {
-	var req RunRequest
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		utils.RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
-		return
-	}
+    var req RunRequest
+    if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+        utils.RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
+        log.Printf("Invalid request payload: %v", err)
+        return
+    }
 
-	if err := daemon.StartContainer(req.Image, req.Command); err != nil {
-		utils.RespondWithError(w, http.StatusInternalServerError, "Failed to start container")
-		return
-	}
+    if err := daemon.StartContainer(req.Image, req.Command); err != nil {
+        utils.RespondWithError(w, http.StatusInternalServerError, "Failed to start container")
+        log.Printf("Failed to start container: %v", err)
+        return
+    }
 
-	utils.RespondWithJSON(w, http.StatusOK, map[string]string{"status": "started"})
+    utils.RespondWithJSON(w, http.StatusOK, map[string]string{"status": "started"})
 }
--- 003 Code/Carte/Carte_Daemon/daemon/container.go
+++ 003 Code/Carte/Carte_Daemon/daemon/container.go
@@ -1,49 +1,94 @@
 package daemon
 
 import (
-	"encoding/json"
-	"net/http"
-	"os/exec"
-	"syscall"
+    "io/ioutil"
+    "os"
+    "os/exec"
+    "path/filepath"
+    "strconv"
+    "syscall"
+    "log"
 )
 
-// StartContainer starts a new container process with namespaces and cgroups
+func setupCgroups() error {
+    cgroups := "/sys/fs/cgroup/"
+    pid := os.Getpid()
+
+    // Memory limit
+    if err := os.MkdirAll(filepath.Join(cgroups, "memory", "carte"), 0755); err != nil {
+        return err
+    }
+    if err := ioutil.WriteFile(filepath.Join(cgroups, "memory", "carte", "memory.limit_in_bytes"), []byte("104857600"), 0700); err != nil {
+        return err
+    }
+    if err := ioutil.WriteFile(filepath.Join(cgroups, "memory", "carte", "cgroup.procs"), []byte(strconv.Itoa(pid)), 0700); err != nil {
+        return err
+    }
+
+    // CPU limit
+    if err := os.MkdirAll(filepath.Join(cgroups, "cpu", "carte"), 0755); err != nil {
+        return err
+    }
+    if err := ioutil.WriteFile(filepath.Join(cgroups, "cpu", "carte", "cpu.cfs_quota_us"), []byte("50000"), 0700); err != nil {
+        return err
+    }
+    if err := ioutil.WriteFile(filepath.Join(cgroups, "cpu", "carte", "cgroup.procs"), []byte(strconv.Itoa(pid)), 0700); err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func setNamespacesAndCgroups(command string) *exec.Cmd {
+    cmd := exec.Command("sh", "-c", command)
+    cmd.SysProcAttr = &syscall.SysProcAttr{
+        Cloneflags: syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET, // 필요한 네임스페이스만 설정
+    }
+    cmd.Env = append(os.Environ(), "PATH=/usr/sbin:/usr/bin:/sbin:/bin")
+
+    return cmd
+}
+
 func StartContainer(image, command string) error {
-	cmd := exec.Command("/proc/self/exe", "child", command)
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET | syscall.CLONE_NEWUSER,
-		UidMappings: []syscall.SysProcIDMap{
-			{ContainerID: 0, HostID: syscall.Getuid(), Size: 1},
-		},
-		GidMappings: []syscall.SysProcIDMap{
-			{ContainerID: 0, HostID: syscall.Getgid(), Size: 1},
-		},
-	}
+    cmd := setNamespacesAndCgroups(command)
 
-	if err := cmd.Start(); err != nil {
-		return err
-	}
+    if err := setupCgroups(); err != nil {
+        log.Printf("Error setting up cgroups: %v", err)
+        return err
+    }
 
-	if err := cmd.Wait(); err != nil {
-		return err
-	}
+    stdout, err := cmd.StdoutPipe()
+    if err != nil {
+        log.Printf("Error creating stdout pipe: %v", err)
+        return err
+    }
+    stderr, err := cmd.StderrPipe()
+    if err != nil {
+        log.Printf("Error creating stderr pipe: %v", err)
+        return err
+    }
 
-	return nil
+    if err := cmd.Start(); err != nil {
+        log.Printf("Error starting command: %v", err)
+        return err
+    }
+
+    go func() {
+        out, _ := ioutil.ReadAll(stdout)
+        log.Printf("Command output: %s", out)
+    }()
+
+    go func() {
+        errOut, _ := ioutil.ReadAll(stderr)
+        log.Printf("Command error output: %s", errOut)
+    }()
+
+    if err := cmd.Wait(); err != nil {
+        log.Printf("Error waiting for command: %v", err)
+        return err
+    }
+
+    return nil
 }
 
-// RunContainer handles the /run endpoint request
-func RunContainer(w http.ResponseWriter, r *http.Request) {
-	var req RunRequest
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
 
-	if err := StartContainer(req.Image, req.Command); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"status": "started"})
-}
